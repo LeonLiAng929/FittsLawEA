5748a440bb6db2992875bbba5e1383c00405a3ad
Leon Li Ang <49157155+LeonLiAng929@users.noreply.github.com>
2025-07-06 21:24:06 +1000

    ViconNetworkTransmission

---
+++ Assets/Scripts/ViconPC/Client.cs
@@ -0,0 +1,66 @@
+using TMPro;
+using UnityEngine;
+using Unity.Networking.Transport;
+
+
+
+public class ClientBehaviour : MonoBehaviour
+{
+    NetworkDriver m_Driver;
+    NetworkConnection m_Connection;
+    //public TMP_Text logMsg;
+    void Start()
+    {
+        m_Driver = NetworkDriver.Create();
+
+        var endpoint = NetworkEndpoint.LoopbackIpv4.WithPort(7777);
+        m_Connection = m_Driver.Connect(endpoint);
+    }
+
+    void OnDestroy()
+    {
+        m_Driver.Dispose();
+    }
+
+    void Update()
+    {
+        m_Driver.ScheduleUpdate().Complete();
+        
+        if (!m_Connection.IsCreated)
+        {
+            var endpoint = NetworkEndpoint.LoopbackIpv4.WithPort(7777);
+            m_Connection = m_Driver.Connect(endpoint);
+            //logMsg.text="No Connection Found, Try Connecting...";
+            return;
+        }
+        //logMsg.text=NetworkEndpoint.LoopbackIpv4.Address;
+        
+        Unity.Collections.DataStreamReader stream;
+        NetworkEvent.Type cmd;
+        while ((cmd = m_Connection.PopEvent(m_Driver, out stream)) != NetworkEvent.Type.Empty)
+        {
+            if (cmd == NetworkEvent.Type.Connect)
+            {
+                Debug.Log("We are now connected to the server.");
+                //logMsg.text="We are now connected to the server.";
+                uint value = 1;
+                m_Driver.BeginSend(m_Connection, out var writer);
+                writer.WriteUInt(value);
+                m_Driver.EndSend(writer);
+            }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                uint value = stream.ReadUInt();
+                Debug.Log($"Got the value {value} back from the server.");
+                //logMsg.text=$"Got the value {value} back from the server.";
+                //m_Connection.Disconnect(m_Driver);
+                //m_Connection = default;
+            }
+            else if (cmd == NetworkEvent.Type.Disconnect)
+            {
+                Debug.Log("Client got disconnected from server.");
+                m_Connection = default;
+            }
+        }
+    }
+}
